478cc390be034169fa01511e9ca6a9f22181029e
Andrew Capp <acapp909@gmail.com>
2019-12-28 01:02:18 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,6 +4,7 @@ class Show < ActiveRecord::Base
 
   def actors_list
     self.characters.collect do |character|
+      binding.pry
       full_name_return = "#{character.actor.first_name} #{character.actor.last_name}"
     end
   end
