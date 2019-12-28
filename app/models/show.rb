a03bd05ad3554ab2152e0870c240b8cfeaa5a682
Andrew Capp <acapp909@gmail.com>
2019-12-28 01:00:34 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,11 @@
 class Show < ActiveRecord::Base
-  
+  belongs_to :network
+  has_many :characters
+
+  def actors_list
+    self.characters.collect do |character|
+      #binding.pry
+      full_name_return = "#{character.actor.first_name} #{character.actor.last_name}"
+    end
+  end
 end
